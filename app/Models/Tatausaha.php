2ff454f8c77240c82169ecedb8794dbeaa65897d
selpi09 <73418289+selpi09@users.noreply.github.com>
2026-02-09 08:27:50 +0700

    Add files via upload

---
+++ app/Models/Tatausaha.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tatausaha extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['pertanyaan', 'jawaban'];
+}
